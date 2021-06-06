5b86ed394c02919e9e2f1c84a3b2179a56284f1b
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-06-06 21:44:22 +0530

    set static assets folder

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use('/api/todos', todoRoutes);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
